67ddebb095e75adf4f991032efe80a23171aabb4
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-09-23 22:56:58 -0300

    agregué componentes ListaTarea e ItemTarea

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap'
+import ListaTarea from './ListaTarea';
 
 const FormularioTareas = () => {
     return (
-        <Form>
-            <Form.Group className='d-flex'>
-                <Form.Control className='me-2' type='tarea' placeholder='Tarea...' />
-                <Button variant='success' type='submit'>Agregar</Button>
-            </Form.Group>
-        </Form>
+        <>
+            <Form className='my-5'>
+                <Form.Group className='d-flex'>
+                    <Form.Control className='me-2' type='tarea' placeholder='Tarea...' />
+                    <Button variant='success' type='submit'>Agregar</Button>
+                </Form.Group>
+            </Form>
+            <ListaTarea></ListaTarea>
+        </>
     );
 };
 
