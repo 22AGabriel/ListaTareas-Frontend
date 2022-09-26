7a98a5f842953b5bb8a4ffa2153b666fada7ebec
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-09-26 15:14:00 -0300

    se guardan las tareas en ArregloTareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap'
 import ListaTarea from './ListaTarea';
 
 const FormularioTareas = () => {
+    const [tarea, setTarea] = useState('');
+    const [arregloTareas, setArregloTareas] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setArregloTareas([...arregloTareas, tarea]);
+        setTarea('');
+    }
+
+
     return (
         <>
-            <Form className='my-5'>
+            <Form className='my-5' onSubmit={handleSubmit}>
                 <Form.Group className='d-flex'>
-                    <Form.Control className='me-2' type='tarea' placeholder='Tarea...' />
+                    <Form.Control 
+                    className='me-2' 
+                    type='tarea' 
+                    placeholder='Tarea...' 
+                    onChange={(e) => setTarea(e.target.value)}
+                    value={tarea}
+                    />
                     <Button variant='success' type='submit'>Agregar</Button>
                 </Form.Group>
             </Form>
