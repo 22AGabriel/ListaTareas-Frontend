8f9e6623c3537aa9b3ac404f8264808476b0bf77
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-04 23:09:48 -0300

    logica para agregar una tarea

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,46 +1,55 @@
-import { useState, useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap'
-import ListaTarea from './ListaTarea';
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { crearTareaAPI } from "./helpers/queries";
+import ListaTarea from "./ListaTarea";
 
 const FormularioTareas = () => {
-    const tareasLocalStorage = JSON.parse(localStorage.getItem('listaTareasStorage')) || [];
-    const [tarea, setTarea] = useState('');
-    const [arregloTareas, setArregloTareas] = useState(tareasLocalStorage);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-    useEffect(() => {
-        localStorage.setItem('listaTareasStorage', JSON.stringify(arregloTareas))
-    }, [arregloTareas])
+  const borrarTarea = (nombre) => {
+    // let arregloModificado = arregloTareas.filter((item) => item !== nombre);
+    // setArregloTareas(arregloModificado);
+  };
 
+  const onSubmit = (data) => {
+    crearTareaAPI(data)
+    reset()
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setArregloTareas([...arregloTareas, tarea]);
-        setTarea('');
-    }
-
-    const borrarTarea = (nombre) => {
-        let arregloModificado = arregloTareas.filter((item) => item !== nombre);
-        setArregloTareas(arregloModificado);
-    }
-
-    return (
-        <>
-            <Form className='my-5' onSubmit={handleSubmit}>
-                <Form.Group className='d-flex'>
-                    <Form.Control 
-                    className='me-2' 
-                    type='tarea' 
-                    placeholder='Tarea...' 
-                    onChange={(e) => setTarea(e.target.value)}
-                    value={tarea}
-                    minLength={5}
-                    />
-                    <Button variant='success' type='submit'>Agregar</Button>
-                </Form.Group>
-            </Form>
-            <ListaTarea arregloTareas={arregloTareas} borrarTarea={borrarTarea}></ListaTarea>
-        </>
-    );
+  return (
+    <>
+      <Form className="my-5" onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="d-flex">
+          <Form.Control
+            className="me-2"
+            type="tarea"
+            placeholder="Tarea..."
+            {...register("nombreTarea", {
+              required: "campo obligatorio",
+              minLength: {
+                value: 5,
+                message: "El mínimo de caracteres es de 5",
+              },
+            })}
+          />
+          <Button variant="success" type="submit">
+            Agregar
+          </Button>
+        </Form.Group>
+        <Form.Group>
+          <Form.Text className="text-danger">
+            {errors.nombreTarea?.message}
+          </Form.Text>
+        </Form.Group>
+      </Form>
+      <ListaTarea borrarTarea={borrarTarea}></ListaTarea>
+    </>
+  );
 };
 
 export default FormularioTareas;
--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -10,3 +10,19 @@ export const consultarAPI = async() => {
         return false;
     }
 }
+
+export const crearTareaAPI = async(tarea) => {
+    try {
+        const respuesta = await fetch(URL, {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(tarea)
+        });
+        return respuesta;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
